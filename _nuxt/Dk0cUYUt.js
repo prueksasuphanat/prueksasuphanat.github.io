e765b50280b355b692914ac4271ee54991eda2e8
Suphanat Panyakom <suphanat.pruek@gmail.com>
2025-02-10 22:19:46 +0700

    nuxt only

---
+++ _nuxt/Dk0cUYUt.js
@@ -0,0 +1,1 @@
+import{v as e,x as a,A as t,O as s,aj as c}from"./DINBRRAG.js";const o={class:"container"},l={__name:"default",setup(r){return(n,_)=>(a(),e("div",o,[t(s(c))]))}};export{l as default};
