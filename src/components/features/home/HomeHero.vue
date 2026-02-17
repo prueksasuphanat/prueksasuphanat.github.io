c1fee67a52e80591abe2837abdb67a5bd7637b86
pruek <suphanat.pruek@gmail.com>
2026-02-18 02:39:03 +0700

    style(home): refactor profile image to use img element
    - Remove background image CSS properties from .home__img container
    - Add flexbox centering to .home__img for proper alignment
    - Replace CSS background image with img element in HomeHero component
    - Add scoped styles for .home__img-element with object-fit cover
    - Improve semantic HTML and image accessibility with alt text
    - Maintain existing animation and dimensions of profile container

--- src/components/features/home/HomeHero.vue
+++ src/components/features/home/HomeHero.vue
@@ -6,7 +6,13 @@
           <SocialLinks />
         </div>
 
-        <div class="home__img"></div>
+        <div class="home__img">
+          <img 
+            :src="profileImage" 
+            alt="Profile" 
+            class="home__img-element"
+          />
+        </div>
 
         <div class="home__data">
           <h1 class="home__title">
@@ -30,4 +36,13 @@ import { personalInfo } from '@/data/about'
 import SocialLinks from '@/components/common/SocialLinks.vue'
 
 const { t } = useI18n()
+const profileImage = '/images/83908473_10213544029074306_1031853080976556032_n.jpg'
 </script>
+
+<style scoped>
+.home__img-element {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+}
+</style>
